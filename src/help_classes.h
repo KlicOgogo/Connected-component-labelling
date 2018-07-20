474265abb4b6c23ecacc841a54887d09f72c3b45
KlicOgogo <baklagan96@gmail.com>
2018-07-20 16:54:01 +0300

    Added methods for implementation

--- src/help_classes.h
+++ src/help_classes.h
@@ -6,10 +6,18 @@ public:
     Point() = default;
     Point(int x, int y): x_(x), y_(y_) {}
     Point(const Point &p) = default;
-    int x() {
+    Point& operator=(const Point &p) {
+        x_ = p.x();
+        y_ = p.y();
+        return *this;
+    }
+    bool operator !=(const Point &p) const {
+        return (x_ != p.x()) || (y_ != p.y());
+    }
+    int x() const {
         return x_;
     }
-    int y() {
+    int y() const {
         return y_;
     }
     void setX(int x) {
@@ -29,6 +37,23 @@ public:
               const Point &right_bottom): left_top_(left_top),
                                           right_bottom_(right_bottom) {}
     Rectangle(const Rectangle &r) = default;
+    Rectangle& operator=(const Rectangle &r) {
+        left_top_ = r.left_top();
+        right_bottom_ = r.right_bottom();
+        return *this;
+    }
+    Point left_top() const {
+        return left_top_;
+    }
+    Point right_bottom() const {
+        return right_bottom_;
+    }
+    void set_left_top(const Point &left_top) {
+        left_top_ = left_top;
+    }
+    void set_right_bottom(const Point &right_bottom) {
+        right_bottom_ = right_bottom;
+    }
 private:
     Point left_top_, right_bottom_;
 };
@@ -38,6 +63,19 @@ public:
     ComponentData() = default;
     ComponentData(Rectangle border, int number): border_(border),
                                                  number_(number) {}
+
+    Rectangle border() const {
+        return border_;
+    }
+    int number() const {
+        return number_;
+    }
+    void set_border(const Rectangle &border) {
+        border_ = border;
+    }
+    void set_number(const int &number) {
+        number_ = number;
+    }
 private:
     Rectangle border_;
     int number_;

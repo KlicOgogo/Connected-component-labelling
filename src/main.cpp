83f303c9d2ea4fb7669e676320550b2f8da0f9c1
KlicOgogo <baklagan96@gmail.com>
2018-07-21 16:55:57 +0300

    Test runner

---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#include <fstream>
+#include "test.h"
+
+int main() {
+    /*
+    std::chrono::milliseconds a(10), b(100), c(1000);
+    std::cout << ((a + b) + c).count() << '\n';
+    std::ifstream test_image("test.txt");
+    std::vector<std::vector<char>> data;
+    cv::Mat chmo(10, 10, CV_8UC1);
+    data.reserve(100);
+    auto im = generate_image(0.5);
+    std::string str;
+    int i = 0;
+    while(test_image >> str) {
+        data.emplace_back(std::vector<char>());
+        data.back().resize(str.size());
+        for (int j = 0; j < str.size(); ++j) {
+            data[i][j] = str[j] - '0';
+            chmo.at<uchar>(i, j) = static_cast<uchar>(data[i][j]);
+        }
+        ++i;
+    }
+    cv::Mat labels, centroids, stats;
+    cv::connectedComponentsWithStats(chmo, labels, stats, centroids);
+    std::deque<ComponentData> comp_data;
+    one_component_at_a_time(im, comp_data);
+    cv::imshow("components", im);
+    cv::waitKey(0);
+    return 0;
+    */
+    bmark::test(0);
+    bmark::test(1);
+    bmark::test(2);
+    bmark::test(3);
+    bmark::test(0, 4);
+    bmark::test(1, 4);
+    bmark::test(2, 4);
+    bmark::test(3, 4);
+}

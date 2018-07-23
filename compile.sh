6a442b2563fc029d8e5161bb9537608c0cae1d77
KlicOgogo <baklagan96@gmail.com>
2018-07-23 15:20:45 +0300

    O2 optimizer

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-g++ -O3 -std=c++14 src/main.cpp -o src/cmake-build-debug/app `pkg-config --cflags --libs opencv`
+g++ -O2 -std=c++14 src/main.cpp -o src/cmake-build-debug/app `pkg-config --cflags --libs opencv`

6268e57f72b5956803b74cb0cc7fd9652cea699f
KlicOgogo <baklagan96@gmail.com>
2018-07-24 19:11:51 +0300

    Better bfs for 8-connectivity

--- src/one_comp.h
+++ src/one_comp.h
@@ -13,7 +13,7 @@ inline void push_and_update(cv::Mat &image, std::queue<cv::Point2i> &q,
 }
 
 inline void push_and_update_left_border(cv::Mat &image, std::queue<cv::Point2i> &q,
-										ComponentData &data, const cv::Point &p, const uchar &value) noexcept {
+                                        ComponentData &data, const cv::Point &p, const uchar &value) noexcept {
     push_and_update(image, q, p, value);
     if (p.y < data.left) {
         data.left = p.y;
@@ -52,7 +52,7 @@ inline void bfs_step1d4(cv::Mat &image, std::queue<cv::Point2i> &q,
     if (cur.x > image.size().width - 1) {
         if (image.at<uchar>(cur.x - image.size().width) == 1) {
             push_and_update_top_border(image, q, data,
-									   cv::Point2i(cur.x - image.size().width, cur.y), value);
+                                       cv::Point2i(cur.x - image.size().width, cur.y), value);
         }
         if (cur.y > 0 && image.at<uchar>(cur.x - 1) == 1) {
             push_and_update_left_border(image, q, data, cv::Point2i(cur.x - 1, cur.y - 1), value);
@@ -64,8 +64,8 @@ inline void bfs_step1d4(cv::Mat &image, std::queue<cv::Point2i> &q,
     }
     if (cur.x + image.size().width < out) {
         if (image.at<uchar>(cur.x + image.size().width) == 1) {
-            push_and_update_bottom_border(image, q, data, 
-										  cv::Point2i(cur.x + image.size().width, cur.y), value);
+            push_and_update_bottom_border(image, q, data,
+                                          cv::Point2i(cur.x + image.size().width, cur.y), value);
         }
         if (cur.y < image.size().width - 1 && image.at<uchar>(cur.x + 1) == 1) {
             push_and_update_right_border(image, q, data, cv::Point2i(cur.x + 1, cur.y + 1), value);
@@ -77,121 +77,57 @@ inline void bfs_step1d4(cv::Mat &image, std::queue<cv::Point2i> &q,
     }
 }
 
-inline void bfs_step_left8(cv::Mat &image, std::queue<cv::Point2i> &q,
-                          ComponentData &data, const uchar &value,
-                          const int &out, const cv::Point2i &cur) noexcept {
-    auto n = image.size().width;
-    if (cur.x > n) {
-        if (cur.y > 0) {
-            if (image.at<uchar>(cur.x - n - 1) == 1) {
-                push_and_update_top_border(image, q, data, cv::Point2i(cur.x - n - 1, cur.y - 1), value);
-                if (cur.y - 1 < data.left) {
-                    data.left = cur.y - 1;
-                }
-            }
-            if (image.at<uchar>(cur.x - 1) == 1) {
-                push_and_update_left_border(image, q, data, cv::Point(cur.x - 1, cur.y - 1), value);
-            }
-        }
-        if (image.at<uchar>(cur.x - n) == 1) {
-            push_and_update_top_border(image, q, data, cv::Point2i(cur.x - n, cur.y), value);
-        }
-        if (cur.y < n - 1 && image.at<uchar>(cur.x - n + 1) == 1) {
-            push_and_update_top_border(image, q, data, cv::Point(cur.x - n + 1, cur.y + 1), value);
-            if (cur.y + 1 > data.right) {
-                data.right = cur.y + 1;
-            }
-        }
-    } else if (cur.x > n - 1) {
-        if (image.at<uchar>(cur.x - n) == 1) {
-            push_and_update_top_border(image, q, data, cv::Point2i(cur.x - n, cur.y), value);
-        }
-        if (cur.y < n - 1 && image.at<uchar>(cur.x - n + 1) == 1) {
-            push_and_update_top_border(image, q, data, cv::Point(cur.x - n + 1, cur.y + 1), value);
-            if (cur.y + 1 > data.right) {
-                data.right = cur.y + 1;
-            }
-        }
-        if (cur.y > 0 && image.at<uchar>(cur.x - 1) == 1) {
-            push_and_update_left_border(image, q, data, cv::Point(cur.x - 1, cur.y - 1), value);
-        }
-    } else if (cur.x > n - 2) {
-        if (cur.y < n - 1 && image.at<uchar>(cur.x - n + 1) == 1) {
-            push_and_update_top_border(image, q, data, cv::Point(cur.x - n + 1, cur.y + 1), value);
-            if (cur.y + 1 > data.right) {
-                data.right = cur.y + 1;
-            }
-        }
-        if (cur.y > 0 && image.at<uchar>(cur.x - 1) == 1) {
-            push_and_update_left_border(image, q, data, cv::Point(cur.x - 1, cur.y - 1), value);
-        }
-    } else if (cur.y > 0 && image.at<uchar>(cur.x - 1) == 1) {
-        push_and_update_left_border(image, q, data, cv::Point(cur.x - 1, cur.y - 1), value);
-    }
-}
-
-inline void bfs_step_right8(cv::Mat &image, std::queue<cv::Point2i> &q,
-                          ComponentData &data, const uchar &value,
-                          const int &out, const cv::Point2i &cur) noexcept {
-    auto n = image.size().width;
-    if (cur.x + n + 1 < out) {
-        if (cur.y < n - 1) {
-            if (image.at<uchar>(cur.x + n + 1) == 1) {
-                push_and_update_bottom_border(image, q, data, cv::Point2i(cur.x + n + 1, cur.y + 1), value);
-                if (cur.y + 1 > data.right) {
-                    data.right = cur.y + 1;
-                }
-            }
-            if (image.at<uchar>(cur.x + 1) == 1) {
-                push_and_update_right_border(image, q, data, cv::Point2i(cur.x + 1, cur.y + 1), value);
-            }
-        }
-        if (image.at<uchar>(cur.x + n) == 1) {
-            push_and_update_bottom_border(image, q, data, cv::Point(cur.x + n, cur.y), value);
-        }
-        if (cur.y > 0 && image.at<uchar>(cur.x + n - 1) == 1) {
-            push_and_update_bottom_border(image, q, data, cv::Point(cur.x + n - 1, cur.y - 1), value);
-            if (cur.y - 1 < data.left) {
-                data.left = cur.y - 1;
-            }
-        }
-    } else if (cur.x + n < out) {
-        if (image.at<uchar>(cur.x + n) == 1) {
-            push_and_update_bottom_border(image, q, data, cv::Point(cur.x + n, cur.y), value);
-        }
-        if (cur.y > 0 && image.at<uchar>(cur.x + n - 1) == 1) {
-            push_and_update_bottom_border(image, q, data, cv::Point(cur.x + n - 1, cur.y - 1), value);
-            if (cur.y - 1 < data.left) {
-                data.left = cur.y - 1;
-            }
-        }
-        if (cur.y < n - 1  && image.at<uchar>(cur.x + 1) == 1) {
-            push_and_update_right_border(image, q, data, cv::Point2i(cur.x + 1, cur.y + 1), value);
-        }
-    } else if (cur.x + n - 1 < out) {
-        if (cur.y > 0 && image.at<uchar>(cur.x + n - 1) == 1) {
-            push_and_update_bottom_border(image, q, data, cv::Point(cur.x + n - 1, cur.y - 1), value);
-            if (cur.y - 1 < data.left) {
-                data.left = cur.y - 1;
-            }
-        }
-        if (cur.y < n - 1  && image.at<uchar>(cur.x + 1) == 1) {
-            push_and_update_right_border(image, q, data, cv::Point2i(cur.x + 1, cur.y + 1), value);
-        }
-    } else if (cur.y < n - 1  && image.at<uchar>(cur.x + 1) == 1) {
-        push_and_update_right_border(image, q, data, cv::Point2i(cur.x + 1, cur.y + 1), value);
-    }
-}
-
 inline void bfs_step1d8(cv::Mat &image, std::queue<cv::Point2i> &q,
-                      ComponentData &data, const uchar &value, const int &out) noexcept {
-    bfs_step_left8(image, q, data, value, out, q.front());
-    bfs_step_right8(image, q, data, value, out, q.front());
+                        ComponentData &data, const uchar &value, const int &out) noexcept {
+    auto cur = q.front();
     q.pop();
+    auto n = image.size().width;
+    if (cur.y > 0) {
+        auto temp_y = cur.y - 1;
+        if (image.at<uchar>(cur.x - 1) == 1) {
+            push_and_update_left_border(image, q, data, cv::Point2i(cur.x - 1, temp_y), value);
+        }
+        if (cur.x > n && image.at<uchar>(cur.x - n - 1) == 1) {
+            push_and_update_top_border(image, q, data, cv::Point2i(cur.x - 1 - n, temp_y), value);
+            if (temp_y < data.left) {
+                data.left = temp_y;
+            }
+        }
+        if (cur.x + n <= out && image.at<uchar>(cur.x + n - 1) == 1) {
+            push_and_update_bottom_border(image, q, data, cv::Point2i(cur.x - 1 + n, temp_y), value);
+            if (temp_y < data.left) {
+                data.left = temp_y;
+            }
+        }
+    }
+    if (cur.y < n - 1) {
+        auto temp_y = cur.y + 1;
+        if (image.at<uchar>(cur.x + 1) == 1) {
+            push_and_update_right_border(image, q, data, cv::Point2i(cur.x + 1, temp_y), value);
+        }
+        if (cur.x > n - 2 && image.at<uchar>(cur.x + 1 - n) == 1) {
+            push_and_update_top_border(image, q, data, cv::Point2i(cur.x + 1 - n, temp_y), value);
+            if (temp_y > data.right) {
+                data.right = temp_y;
+            }
+        }
+        if (cur.x + n + 1 < out && image.at<uchar>(cur.x + n + 1) == 1) {
+            push_and_update_bottom_border(image, q, data, cv::Point2i(cur.x + 1 + n, temp_y), value);
+            if (temp_y > data.right) {
+                data.right = temp_y;
+            }
+        }
+    }
+    if (cur.x >= n && image.at<uchar>(cur.x - n) == 1) {
+        push_and_update_top_border(image, q, data, cv::Point2i(cur.x - n, cur.y), value);
+    }
+    if (cur.x + n < out && image.at<uchar>(cur.x + n) == 1) {
+        push_and_update_bottom_border(image, q, data, cv::Point2i(cur.x + n, cur.y), value);
+    }
 }
 
 void one_component_at_a_time1d(cv::Mat &image, std::deque<ComponentData> &data,
-                             int connectivity = 8) noexcept {
+                               int connectivity = 8) noexcept {
     int out = image.size().width * image.size().height, n = image.size().width;
     data = {};
     std::queue<cv::Point2i> q;

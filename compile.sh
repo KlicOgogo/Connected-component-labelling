d8be596e9a57644e73f2e429d0306b652c916a8b
KlicOgogo <baklagan96@gmail.com>
2018-07-24 23:33:00 +0300

    Updated console script

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-g++ -O2 -std=c++14 src/main.cpp -o src/cmake-build-debug/app `pkg-config --cflags --libs opencv`
+g++ -O2 -std=c++14 src/algorithm.cpp src/image_generators.cpp src/main.cpp -o src/cmake-build-debug/app `pkg-config --cflags --libs opencv`
